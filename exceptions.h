df013c1239585965e298f1bcb79330e5fc225ede
Boris Timofeenko <tmfn@feip.co>
2018-11-20 13:12:23 +1000

    Rewrite exceptions

--- exceptions.h
+++ exceptions.h
@@ -5,54 +5,41 @@ protected:
     std::string m_msg;
 public:
 
-    LexerException(int line, int column) {
-        m_msg = ("LexerError at line "+std::to_string(line)+" column "+std::to_string(column));
+    LexerException(int line, int column, std::string msg = "") {
+        m_msg = "LexerError: " + msg +  " at line " + std::to_string(line) + " column " + std::to_string(column);
     }
 
-    virtual const char* what() const throw()
-    {
+    virtual const char *what() const throw() {
         return m_msg.c_str();
     }
 };
 
 class IncorrectOperatorException : public LexerException {
 public:
-    IncorrectOperatorException(int line, int column) : LexerException(line, column) {
-        m_msg = ("LexerError: Incorrect operator at line "+std::to_string(line)+" column "+std::to_string(column));
-    }
+    IncorrectOperatorException(int line, int column) : LexerException(line, column, "Incorrect operator") {}
 };
 
 class IncorrectCharacterException : public LexerException {
 public:
-    IncorrectCharacterException(int line, int column) : LexerException(line, column) {
-        m_msg = "LexerError: Incorrect character at line "+std::to_string(line)+" column "+std::to_string(column);
-    }
+    IncorrectCharacterException(int line, int column) : LexerException(line, column,"Incorrect character") {}
 };
 
 class UnterminatedStringException : public LexerException {
 public:
-    UnterminatedStringException(int line, int column) : LexerException(line, column) {
-        m_msg = ("LexerError: Unterminated string at line "+std::to_string(line)+" column "+std::to_string(column));
-    }
+    UnterminatedStringException(int line, int column) : LexerException(line, column,"Unterminated string") {}
 };
 
 class UnterminatedCommentException : public LexerException {
 public:
-    UnterminatedCommentException(int line, int column) : LexerException(line, column) {
-        m_msg = ("LexerError: Unterminated comment at line "+std::to_string(line)+" column "+std::to_string(column));
-    }
+    UnterminatedCommentException(int line, int column) : LexerException(line, column,"Unterminated comment") {}
 };
 
 class OutOfRangeNumberException : public LexerException {
 public:
-    OutOfRangeNumberException(int line, int column) : LexerException(line, column) {
-        m_msg = ("LexerError: Out of range at line "+std::to_string(line)+" column "+std::to_string(column));
-    }
+    OutOfRangeNumberException(int line, int column) : LexerException(line, column,"Out of range") {}
 };
 
 class IncorrectCommentException : public LexerException {
 public:
-    IncorrectCommentException(int line, int column) : LexerException(line, column) {
-        m_msg = ("LexerError: Incorrect Comment at line "+std::to_string(line)+" column "+std::to_string(column));
-    }
+    IncorrectCommentException(int line, int column) : LexerException(line, column,"Incorrect Comment") {}
 };
